e9a872dfb76bc74b08e012ef0cc27e995ac79b51
DavidBarney <rabidjackalope@gmail.com>
2019-03-29 22:11:47 -0700

    Moved the Menu Displayer from the GeneralAuthority Class to the MainProgram Class

--- src/cit260project/GeneralAuthority.java
+++ src/cit260project/GeneralAuthority.java
@@ -2,13 +2,4 @@ package cit260project;
 
 public class GeneralAuthority {
 
-    public static void main(String[] args) {
-        
-        Menu menu = new MainMenu();
-        
-        menu.display();
-        
-        System.out.println("Good-bye");
-    }
-
 }
---
+++ src/cit260project/MainProgram.java
@@ -0,0 +1,16 @@
+package cit260project;
+
+public class MainProgram {
+    
+    //Testing the Menu Classes.
+    public static void main(String[] args) {
+        
+        Menu menu = new MainMenu();
+        
+        menu.display();
+        
+        System.out.println("Good-bye");
+    }
+
+
+}
