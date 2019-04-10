940eb96318d2afb3e0ece2b60f290f94c14a8453
DavidBarney <rabidjackalope@gmail.com>
2019-04-09 21:52:39 -0700

    Added a subMenu method and sampleFile method for the handleMenuSelection method

--- src/cit260project/MainMenu.java
+++ src/cit260project/MainMenu.java
@@ -6,92 +6,100 @@ import java.util.Scanner;
 
 public class MainMenu extends Menu {
 
-	/**
-	 * Default constructor. Does nothing.
-	 */
-	public MainMenu() {
-		super();
-	}
+    /**
+     * Default constructor. Does nothing.
+     */
+    public MainMenu() {
+        super();
+    }
 
-	/**
-	 * Provide the menu title (or null to skip).
-	 * 
-	 * @return
-	 */
-	@Override
-	protected String getTitle() {
-		return "----------\nMAIN MENU\n----------";
-	}
+    /**
+     * Provide the menu title (or null to skip).
+     * 
+     * @return
+     */
+    @Override
+    protected String getTitle() {
+        return "----------\nMAIN MENU\n----------";
+    }
 
-	/**
-	 * Provide a menu description (or null to skip).
-	 * 
-	 * @return
-	 */
-	@Override
-	protected String getDescription() {
-		return "Welcome to the General Authority Database. \nPlease choose from the options below.";
-	}
+    /**
+     * Provide a menu description (or null to skip).
+     * 
+     * @return
+     */
+    @Override
+    protected String getDescription() {
+        return "Welcome to the General Authority Database. \nPlease choose from the options below.";
+    }
 
-	/**
-	 * Provide the list of menu items.
-	 * 
-	 * @return
-	 */
-	@Override
-	protected MenuItem[] getMenuItems() {
-		return new MenuItem[] { new MenuItem('1', "Prophet"), new MenuItem('2', "Apostle"),
-				new MenuItem('3', "Seventy"), new MenuItem('4', "Sample"), new MenuItem('Q', "Quit Program") };
-	}
+    /**
+     * Provide the list of menu items.
+     * 
+     * @return
+     */
+    @Override
+    protected MenuItem[] getMenuItems() {
+        return new MenuItem[] { new MenuItem('1', "Prophet"), new MenuItem('2', "Apostle"),
+                new MenuItem('3', "Seventy"), new MenuItem('4', "Sample"), new MenuItem('Q', "Quit Program") };
+    }
 
-	/**
-	 * Handle the user's menu selection.
-	 * 
-	 * @return false if the menu should exit and return to whatever called it.
-	 */
-	@Override
-	protected boolean handleMenuSelection(char key) {
+    /**
+     * Handle the user's menu selection.
+     * 
+     * @return false if the menu should exit and return to whatever called it.
+     */
+    @Override
+    protected boolean handleMenuSelection(char key) {
 
-		switch (Character.toUpperCase(key)) {
-		case '1':
-			Menu subMenu = new GenAuthSubMenu();
-			subMenu.display();
-			break;
-		case '2':
-			System.out.println("Place holder for database access method 2");
-			break;
-		case '3':
-			System.out.println("Place holder for database access method 3");
-			break;
-		case '4':
-			File sampleFile = new File("Sample.txt");
-			readFile(sampleFile);
-			break;
+        switch (Character.toUpperCase(key)) {
+        case '1':
+            subMenu();
+            break;
+        case '2':
+            subMenu();
+            break;
+        case '3':
+            subMenu();
+            break;
+        case '4':
+            sampleFile();
+            break;
 
-		case 'Q':
-			return false;
+        case 'Q':
+            return false;
 
-		default:
-			System.err.println("Invalid Entry");
+        default:
+            System.err.println("Invalid Entry");
 
-		}
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	// Method to read from existing file and print the the console
-	public Scanner readFile(File file) {
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(file);
+    private void subMenu() {
+        Menu subMenu = new GenAuthSubMenu();
+        subMenu.display();
+    }
 
-			while (scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Error finding example file.");
-		}
-		return scanner;
-	}
+    private void sampleFile() {
+        File sampleFile = new File("Sample.txt");
+        readFile(sampleFile);
+    }
+
+    // Method to read from existing file and print the the console
+    public Scanner readFile(File file) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error finding example file.");
+        }
+        return scanner;
+    }
 
 }
