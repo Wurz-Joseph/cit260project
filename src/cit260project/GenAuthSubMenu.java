55c4b0581c58c6677be2da784194cb1957e5fab1
Joseph Wurz <wur17002@byui.edu>
2019-04-10 14:51:10 -0500

    added if statement to check if file is empty.

--- src/cit260project/GenAuthSubMenu.java
+++ src/cit260project/GenAuthSubMenu.java
@@ -20,10 +20,10 @@ public class GenAuthSubMenu extends Menu {
 	 * 
 	 * @return
 	 */
-	@Override
-	protected String getTitle() {
-		return "Display General Authorities";
-	}
+	/*
+	 * @Override protected String getTitle() { return "Display General Authorities";
+	 * }
+	 */
 
 	/**
 	 * Provide a menu description (or null to skip).
@@ -43,11 +43,8 @@ public class GenAuthSubMenu extends Menu {
 	@Override
 	protected MenuItem[] getMenuItems() {
 		return new MenuItem[] { new MenuItem('1', "Show list of Prophets"),
-				new MenuItem('2', "Add to the existing file"), new MenuItem('R', "Return to MainMenu") };
+				new MenuItem('2', "Add a Prophet to the file"), new MenuItem('R', "Return to MainMenu") };
 	}
-	
-	
-
 
 	/**
 	 * Handle the user's menu selection.
@@ -59,8 +56,8 @@ public class GenAuthSubMenu extends Menu {
 
 		switch (Character.toUpperCase(key)) {
 		case '1':
-			//load from external file
-			File prophetFile = new File("Prophets.txt");
+			// load from external file
+			File prophetFile = new File("Apostles.txt");
 			readProphet(prophetFile);
 			break;
 		case '2':
@@ -84,26 +81,25 @@ public class GenAuthSubMenu extends Menu {
 	ArrayList<GeneralAuthority> apostles = new ArrayList<GeneralAuthority>();
 
 	public void storeApostles(ArrayList<GeneralAuthority> apostles) {
-		//prompt to add G.A. data and read in values.
+		// prompt to add G.A. data and read in values.
 		System.out.print("Full Name: ");
 		String name = scanner.nextLine();
-		
+
 		System.out.print("Office: ");
 		String office = scanner.nextLine();
-		
+
 		System.out.print("Favorite Quote: ");
 		String favoriteQuote = scanner.nextLine();
-		
-		//create new G.A. object
+
+		// create new G.A. object
 		GeneralAuthority ga = new GeneralAuthority(name, office, favoriteQuote);
-		//add object to apostles arrayList
+		// add object to apostles arrayList
 		apostles.add(ga);
 
-		
 		try {
-			//create/append to Apostles.txt	
-			PrintWriter pw = new PrintWriter(new FileOutputStream(new File(FILEPATH),true));
-			//print to file
+			// create/append to Apostles.txt
+			PrintWriter pw = new PrintWriter(new FileOutputStream(new File(FILEPATH), true));
+			// print to file
 			pw.println(apostles.toString());
 			pw.close();
 		} catch (FileNotFoundException e) {
@@ -132,12 +128,20 @@ public class GenAuthSubMenu extends Menu {
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(file);
-
-			while (scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
+			
+			//if file is empty display error. Else display list of Apostles.
+			if (file.length() == 0) {
+				System.err.println("No prophets currently in the database. Try adding one to the file.");
+			} else {
+				while (scanner.hasNextLine()) {
+					System.out.println(scanner.nextLine());
+				}
 			}
+	
 		} catch (FileNotFoundException e) {
 			System.err.println("Error finding file.");
+			// display menu if file not found
+			display();
 		}
 		return scanner;
 	}
