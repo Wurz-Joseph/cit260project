b7b5fb6d07609bc3ef78ac813d337ca5db339712
DavidBarney <rabidjackalope@gmail.com>
2019-04-10 21:09:09 -0700

    Changed dateOfBirth and dateSustained variables to String type, it's easier to manage and it can be stored in the format the user wants. Adjusted the toString method to include the dates.

--- src/cit260project/GenAuthSubMenu.java
+++ src/cit260project/GenAuthSubMenu.java
@@ -9,163 +9,172 @@ import java.io.File;
 import java.io.PrintWriter;
 
 public class GenAuthSubMenu extends Menu {
-	private final String FILEPATH = "Apostles.txt";
+    private final String FILEPATH = "Apostles.txt";
 
-	public GenAuthSubMenu() {
-		super();
-	}
+    public GenAuthSubMenu() {
+        super();
+    }
 
-	/**
-	 * Provide the menu title (or null to skip).
-	 * 
-	 * @return
-	 */
-	/*
-	 * @Override protected String getTitle() { return "Display General Authorities";
-	 * }
-	 */
+    /**
+     * Provide the menu title (or null to skip).
+     * 
+     * @return
+     */
+    /*
+     * @Override protected String getTitle() { return "Display General Authorities";
+     * }
+     */
 
-	/**
-	 * Provide a menu description (or null to skip).
-	 * 
-	 * @return
-	 */
-	@Override
-	protected String getDescription() {
-		return "Show list or add to file";
-	}
+    /**
+     * Provide a menu description (or null to skip).
+     * 
+     * @return
+     */
+    @Override
+    protected String getDescription() {
+        return "Show list or add to file";
+    }
 
-	/**
-	 * Provide the list of menu items.
-	 * 
-	 * @return
-	 */
-	@Override
-	protected MenuItem[] getMenuItems() {
-		return new MenuItem[] { new MenuItem('1', "Show list of Prophets"),
-				new MenuItem('2', "Add a Prophet to the file"), new MenuItem('R', "Return to MainMenu") };
-	}
+    /**
+     * Provide the list of menu items.
+     * 
+     * @return
+     */
+    @Override
+    protected MenuItem[] getMenuItems() {
+        return new MenuItem[] { new MenuItem('1', "Show list of Prophets"),
+                new MenuItem('2', "Add a Prophet to the file"), new MenuItem('R', "Return to MainMenu") };
+    }
 
-	/**
-	 * Handle the user's menu selection.
-	 * 
-	 * @return false if the menu should exit and return to whatever called it.
-	 */
-	@Override
-	protected boolean handleMenuSelection(char key) {
+    /**
+     * Handle the user's menu selection.
+     * 
+     * @return false if the menu should exit and return to whatever called it.
+     */
+    @Override
+    protected boolean handleMenuSelection(char key) {
 
-		switch (Character.toUpperCase(key)) {
-		case '1':
-			// load from external file
-			File prophetFile = new File("Apostles.txt");
-			readProphet(prophetFile);
-			break;
-		case '2':
-			storeApostles(apostles);
-			break;
-		case 'R':
-			return false;
-		default:
-			System.out.println("Invalid Entry");
+        switch (Character.toUpperCase(key)) {
+        case '1':
+            // load from external file
+            File prophetFile = new File("Apostles.txt");
+            readProphet(prophetFile);
+            break;
+        case '2':
+            storeApostles(apostles);
+            break;
+        case 'R':
+            return false;
+        default:
+            System.out.println("Invalid Entry");
 
-		}
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * store apostles to file
-	 * 
-	 * @param apostles
-	 */
-	ArrayList<GeneralAuthority> apostles = new ArrayList<GeneralAuthority>();
+    /**
+     * store apostles to file
+     * 
+     * @param apostles
+     */
+    ArrayList<GeneralAuthority> apostles = new ArrayList<GeneralAuthority>();
 
-	public void storeApostles(ArrayList<GeneralAuthority> apostles) {
-		// prompt to add G.A. data and read in values.
-		System.out.print("Full Name: ");
-		String name = scanner.nextLine();
+    public void storeApostles(ArrayList<GeneralAuthority> apostles) {
+        try (Scanner input = new Scanner(System.in)) {
 
-		System.out.print("Office: ");
-		String office = scanner.nextLine();
+            // prompt to add G.A. data and read in values.
+            System.out.print("Full Name: ");
+            String name = scanner.nextLine();
 
-		System.out.print("Favorite Quote: ");
-		String favoriteQuote = scanner.nextLine();
+            System.out.print("Office: ");
+            String office = scanner.nextLine();
 
-		// create new G.A. object
-		GeneralAuthority ga = new GeneralAuthority(name, office, favoriteQuote);
-		// add object to apostles arrayList
-		apostles.add(ga);
+            System.out.print("Date of Birth: ");
+            String dateOfBirth = scanner.nextLine();
 
-		try {
-			// create/append to Apostles.txt
-			PrintWriter pw = new PrintWriter(new FileOutputStream(new File(FILEPATH), true));
-			// print to file
-			pw.println(apostles.toString());
-			pw.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+            System.out.print("Date of Sustained: ");
+            String dateSustained = scanner.nextLine();
 
-	/**
-	 * read apostles from file.
-	 * 
-	 * @return
-	 */
-	public ArrayList<GeneralAuthority> loadApostles() {
-		System.out.println(apostles.toString());
+            System.out.print("Favorite Quote: ");
+            String favoriteQuote = scanner.nextLine();
 
-		return apostles;
-	}
+            // create new G.A. object
+            GeneralAuthority ga = new GeneralAuthority(name, office, dateOfBirth, dateSustained, favoriteQuote);
+            // add object to apostles arrayList
+            apostles.add(ga);
+        }
 
-	/**
-	 * Read Prophets from the file
-	 * 
-	 * @param scanner
-	 * @return User input of Prophet
-	 */
-	private Scanner readProphet(File file) {
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(file);
-			
-			//if file is empty display error. Else display list of Apostles.
-			if (file.length() == 0) {
-				System.err.println("No prophets currently in the database. Try adding one to the file.");
-			} else {
-				while (scanner.hasNextLine()) {
-					System.out.println(scanner.nextLine());
-				}
-			}
-	
-		} catch (FileNotFoundException e) {
-			System.err.println("Error finding file.");
-			// display menu if file not found
-			display();
-		}
-		return scanner;
-	}
+        try {
+            // create/append to Apostles.txt
+            PrintWriter pw = new PrintWriter(new FileOutputStream(new File(FILEPATH), true));
+            // print to file
+            pw.println(apostles.toString());
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Read Apostles from file
-	 * 
-	 * @param scanner
-	 * @return User input of Apostle
-	 */
-	private GeneralAuthority readApostle(Scanner scanner) {
-		// will return apostle info
-		return null;
-	}
+    /**
+     * read apostles from file.
+     * 
+     * @return
+     */
+    public ArrayList<GeneralAuthority> loadApostles() {
+        System.out.println(apostles.toString());
 
-	/**
-	 * Read members of Seventy from file
-	 * 
-	 * @param scanner
-	 * @return User input of Seventy
-	 */
-	private GeneralAuthority readSeventy(Scanner scanner) {
-		// will return seventy info
-		return null;
-	}
+        return apostles;
+    }
+
+    /**
+     * Read Prophets from the file
+     * 
+     * @param scanner
+     * @return User input of Prophet
+     */
+    private Scanner readProphet(File file) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+
+            // if file is empty display error. Else display list of Apostles.
+            if (file.length() == 0) {
+                System.err.println("No prophets currently in the database. Try adding one to the file.");
+            } else {
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine());
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Error finding file.");
+            // display menu if file not found
+            display();
+        }
+        return scanner;
+    }
+
+    /**
+     * Read Apostles from file
+     * 
+     * @param scanner
+     * @return User input of Apostle
+     */
+    private GeneralAuthority readApostle(Scanner scanner) {
+        // will return apostle info
+        return null;
+    }
+
+    /**
+     * Read members of Seventy from file
+     * 
+     * @param scanner
+     * @return User input of Seventy
+     */
+    private GeneralAuthority readSeventy(Scanner scanner) {
+        // will return seventy info
+        return null;
+    }
 
 }
--- src/cit260project/GeneralAuthority.java
+++ src/cit260project/GeneralAuthority.java
@@ -1,7 +1,6 @@
 package cit260project;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,89 +10,91 @@ import java.io.FileNotFoundException;
  * 
  * @author
  */
-public class GeneralAuthority extends Menu{
-	private String name;
-	private Calendar dateOfBirth;
-	private String office;
-	private String dateSustained;
-	private String favoriteQuote;
+public class GeneralAuthority extends Menu {
+    private String name;
+    private String dateOfBirth;
+    private String office;
+    private String dateSustained;
+    private String favoriteQuote;
 
-	
+    /**
+     * Constructor for a Person object.
+     * 
+     * @param name        (Cannot be null)
+     * 
+     * @param dateOfBrith (Cannot be null)
+     * 
+     * @throws IllegalArgumentException if any of the parameters is null.
+     */
+    public GeneralAuthority(String name, String office, String dateOfBirth, String dateSustained,
+            String favoriteQuote) {
 
-	/**
-	 * Constructor for a Person object.
-	 * 
-	 * @param name        (Cannot be null)
-	 * 
-	 * @param dateOfBrith (Cannot be null)
-	 * 
-	 * @throws IllegalArgumentException if any of the parameters is null.
-	 */
-	public GeneralAuthority(String name, String office, String favoriteQuote) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.office = office;
+        this.dateSustained = dateSustained;
+        this.favoriteQuote = favoriteQuote;
+        // Enforce the "not null" part of our contract.
+        if (name == null) {
+            throw new IllegalArgumentException("name cannot be null");
+        }
 
-		this.name = name;
-		//this.dateOfBirth = dateOfBirth;
-		this.office = office;
-		//this.dateSustained = dateSustained;
-		this.favoriteQuote = favoriteQuote;
-		// Enforce the "not null" part of our contract.
-		if (name == null) {
-			throw new IllegalArgumentException("name cannot be null");
-		}
+        // if (dateOfBirth == null) {
+        // throw new IllegalArgumentException("dateOfBrith cannot be null");
+        // }
 
-		//if (dateOfBirth == null) {
-		//	throw new IllegalArgumentException("dateOfBrith cannot be null");
-		//}
+    }
 
-	}
+    /**
+     * Provide the person's name
+     * 
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Provide the person's name
-	 * 
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Provide the person's date of birth
+     * 
+     * @return the dateOfBirth
+     */
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
 
-	/**
-	 * Provide the person's date of birth
-	 * 
-	 * @return the dateOfBirth
-	 */
-	public Calendar getDateOfBirth() {
-		return dateOfBirth;
-	}
+    /**
+     * 
+     * @return office (Prophet, Apostle, Seventy)
+     */
+    public String getOffice() {
+        return office;
+    }
 
-	/**
-	 * 
-	 * @return office (Prophet, Apostle, Seventy)
-	 */
-	public String getOffice() {
-		return office;
-	}
-	/**
-	 * 
-	 * @return date sustained
-	 */
-	public String getDateSustained() {
-		return dateSustained;
-	}
+    /**
+     * 
+     * @return date sustained
+     */
+    public String getDateSustained() {
+        return dateSustained;
+    }
 
-	/**
-	 * 
-	 * @return favorite quote
-	 */
-	public String getFavoriteQuote() {
-		return favoriteQuote;
-	}
-	/**
-	 * override the toString method for printing/storing objects to file.
-	 */
-	@Override
-    public String toString() { 
-        return String.format("Full name: " + getName() + "\nOffice: " + getOffice() + "\nFavorite Quote: \""  + getFavoriteQuote() + "\"\n"); 
-    } 
+    /**
+     * 
+     * @return favorite quote
+     */
+    public String getFavoriteQuote() {
+        return favoriteQuote;
+    }
 
+    /**
+     * override the toString method for printing/storing objects to file.
+     */
+    @Override
+    public String toString() {
+        return String.format("Full name: " + getName() + "\nOffice: " + getOffice() + "\nDate of Birth: "
+                + getDateOfBirth() + "\nDate Sustained: " + getDateSustained() + "\nFavorite Quote: \""
+                + getFavoriteQuote() + "\"\n");
+    }
 
 }
